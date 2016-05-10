713ea04969b87e3f7af5993da5cb06313d6a2261
marco <3356410@studenti.unige.it>
2016-05-10 15:39:27 +0200

    prova

---
+++ src/Client.java
@@ -0,0 +1,8 @@
+/**
+ * Created by marco on 10/05/16.
+ */
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,33 @@
+import java.rmi.RemoteException;
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server implements RemoteMethods {
+    ConcurrentHashMap<String, String> utenti = new ConcurrentHashMap<String,String>();
+
+    @Override
+    public synchronized void registration(String username, String password) throws RemoteException {
+
+    }
+
+    @Override
+    public void login(String username, String password) throws RemoteException {
+
+    }
+
+    @Override
+    public void logout() throws RemoteException {
+
+    }
+
+    @Override
+    public void updatePosition() throws RemoteException {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
