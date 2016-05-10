44066f1bbfddb939a3d8c2ae54ef936242f0b8af
marco <3356410@studenti.unige.it>
2016-05-10 18:52:25 +0200

    aggiunto rmi

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,23 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 /**
  * Created by marco on 10/05/16.
  */
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+        try {
+            String name = "Server";
+            Registry registry = LocateRegistry.getRegistry(8081);
+            RemoteMethods remoteMethods = (RemoteMethods) registry.lookup(name);
+            //remoteMethods.registration("marco","azaz");
 
+        } catch (Exception e) {
+            System.err.println("ComputePi exception:");
+            e.printStackTrace();
+        }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,16 +1,23 @@
+import java.net.MalformedURLException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.Naming;
 import java.rmi.RemoteException;
-import java.util.HashMap;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-public class Server implements RemoteMethods {
+
+public class Server implements RemoteMethods  {
     ConcurrentHashMap<String, String> utenti = new ConcurrentHashMap<String,String>();
 
     @Override
     public void registration(String username, String password) throws RemoteException {
-        if(utenti.get(username) == null) utenti.put(username, password);
-        return;//nome utente già in uso
+        if(utenti.get(username) == null){
+            utenti.put(username, password);
+            System.err.println("Aggiunti "+ username + " " + password);
+        }
+        return;//TODO aggiungere eccezione se il nome utente è già in uso
     }
 
     @Override
@@ -29,7 +36,21 @@ public class Server implements RemoteMethods {
     }
 
     public static void main(String[] args) {
-        Server server;
-
+            if (System.getSecurityManager() == null) {
+                System.setSecurityManager(new SecurityManager());
+            }
+            try {
+                String name = "Server";
+                RemoteMethods server = new Server();
+                RemoteMethods stub = (RemoteMethods) UnicastRemoteObject.exportObject(server,0);
+                Registry registry = LocateRegistry.getRegistry();
+                registry.rebind(name, stub);
+                System.out.println("Server bound");
+            }
+            catch (Exception e) {
+                System.err.println("Server exception:");
+                e.printStackTrace();
+            }
     }
 }
+
